bf404c393f647e01f4905a7a78d2d9fbc9357638
Karigia-Ngara <fredkarigia@gmail.com>
2023-03-16 12:15:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,7 +9,13 @@
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
+    char *str = "Programming is like building a multilingual puzzle\n";
+    size_t len = strlen(str);
+    ssize_t bytes_written = write(STDOUT_FILENO, str, len);
+
+    if (bytes_written == -1 || (size_t) bytes_written != len)
+        return (1);
+
     return (0);
 }
 
